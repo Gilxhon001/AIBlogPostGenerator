85bd60906cc2ba5bc4a5f95155408c1f59565f78
Gilxhon <gilxhonhima@gmail.com>
2023-08-27 18:03:29 +0200

    add required routes

---
+++ pages/post/[postId].js
@@ -0,0 +1,7 @@
+export default function Post() {
+  return (
+    <div>
+      <h1>This is the new Post page with id </h1>
+    </div>
+  );
+}
---
+++ pages/token-topup.js
@@ -0,0 +1,7 @@
+export default function TokenTopup() {
+  return (
+    <div>
+      <h1>This is the token top up</h1>
+    </div>
+  );
+}
